4adac27063d78455b07599a51eac7cdc20ef856b
Anthony Albanese <anthonya@legalaidnc.org>
2022-04-25 01:47:11 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.0e38cd7e01fa63e444e91c98efdacb0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "358cfcd2981899af1295",
+    "url": "/mlp-decision-tree/static/css/main.1ddea362.chunk.css"
+  },
+  {
+    "revision": "358cfcd2981899af1295",
+    "url": "/mlp-decision-tree/static/js/main.c2c2cae7.chunk.js"
+  },
+  {
+    "revision": "13f73eec1100796e4655",
+    "url": "/mlp-decision-tree/static/js/runtime~main.cf069657.js"
+  },
+  {
+    "revision": "12e3fa0f8db820ad1256",
+    "url": "/mlp-decision-tree/static/js/2.edf0b40c.chunk.js"
+  },
+  {
+    "revision": "5bdd4bc3aa3848d6070daa365a3aa8a8",
+    "url": "/mlp-decision-tree/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-editable-tree/precache-manifest.fcd4681f91449a8478f75bbdf58c88e7.js"
+  "/mlp-decision-tree/precache-manifest.0e38cd7e01fa63e444e91c98efdacb0e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-editable-tree/index.html", {
+workbox.routing.registerNavigationRoute("/mlp-decision-tree/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
